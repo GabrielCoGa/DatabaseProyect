aee5d5077c5d1b769ab94486346870be0a944d9a
GabrielCoGa <gabriel.cortes@hotmail.com>
2026-03-05 17:49:30 +0100

    select e insert

--- DatabaseMysql/Codetek/Sql/mysql_database.py
+++ DatabaseMysql/Codetek/Sql/mysql_database.py
@@ -13,19 +13,47 @@ def connect(db_name):
         print(f"Error connecting to MySQL: {e}")
 
 def select_all_projects():
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM projects")
     results = cursor.fetchall()
     for row in results:
         print(row)
-    cursor.close()   
+    cursor.close()  
 
-   
+def add_new_project_dupla():
+    cursor = db.cursor()
+    project_data = ("Clean house", "Clean by room")
+    cursor.execute("INSERT INTO projects(title, description) VALUES (%s, %s)", project_data)
+
+    project_id = cursor.lastrowid
+
+    tasks_data = [
+        (project_id, "Clean kitchen"),
+        (project_id, "Clean living room")
+    ]
+    cursor.executemany('''INSERT INTO tasks (project_id, description)
+                        VALUES (%s, %s)''', tasks_data)
+    db.commit()
+
+def add_new_project():
+    name = input("Enter project name: ")
+    description = input("Enter project description: ")
+    add_new_project_param(name, description) 
+
+def add_new_project_param(name, description):
+    cursor = db.cursor()
+    sql = "INSERT INTO projects (name, description) VALUES (%s, %s)"
+    val = (name, description)
+    cursor.execute(sql, val)
+    db.commit()
+    print(f"Project '{name}' added successfully.")   
 
 if __name__ == "__main__":
     # Connect to the database
     db = connect("projects")
-    cursor = db.cursor()
-    select_all_projects()
+    #cursor = db.cursor() una vez usado el cursor se cierra, por eso se vuelve a crear cada vez que se necesita
+    #select_all_projects()
+    add_new_project_dupla()
     db.close()
 
 
